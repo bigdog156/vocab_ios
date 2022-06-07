e5915ed5277139531f2ce7c7f0eb296eb21250a7
lamle <16521516@gm.uit.edu.vn>
2022-06-07 12:38:56 +0700

    Initial Commit

---
+++ vocabproject/vocabprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  vocabprojectApp.swift
+//  vocabproject
+//
+//  Created by Developer on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct vocabprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
